b3d40c800736ae4bb66fde8de885642b20a93be5
lily <goldner@oxy.edu>
2016-04-14 11:23:01 -0700

    made a whole directory would you look at that

--- flask-app.py
+++ flask-app.py
@@ -12,14 +12,14 @@ def view_hello():
 def view_demo_1():
     return render_template('demo-1.html', name='Justin')
 
-@app.route('/demo-2/<name>')
-def view_demo_2(name):
-    return render_template('demo-1.html', name=name)
+@app.route('/demo-2/<word>')
+def view_demo_2(word):
+    return render_template('demo-1.html', name=word)
 
 @app.route('/demo-3')
 def view_demo_3():
-    names = ['Alice', 'Bob', 'Charlie']
-    return render_template('demo-3.html', salutation='Roll call', names=names)
+    asf = ['Alice', 'Bob', 'Charlie']
+    return render_template('demo-3.html', salutation='Roll call', names=asf)
 
 # STUDENT DIRECTORY APP
 
@@ -42,11 +42,26 @@ def get_data():
 
 @app.route('/directory')
 def view_directory():
-    return 'FIXME'
+    list = get_data()
+    return render_template('directory.html', students = list)
 
 @app.route('/directory/<username>')
 def view_student(username):
-    return 'FIXME'
+    my_list = get_data()
+    for student in my_list:
+        if student.username == username:
+            student = student
+            number = my_list.index(student)
+            if my_list.index(student) == 0:
+                previous_student = my_list[-1]
+                next_student = my_list[number + 1]
+            elif my_list.index(student) == (len(my_list) - 1):
+                next_student = my_list[0]
+                previous_student = my_list[number - 1]
+            else:
+                previous_student = my_list[number - 1]
+                next_student = my_list[number + 1]
+            return render_template('student.html', student = student, prev_student = previous_student, next_student = next_student )
 
 # DON'T TOUCH THE CODE BELOW THIS LINE
 
